c6955ea0503e2c0dc24195b6c52f3d958851d0b6
Timothy McFadden <tim@timandjamie.com>
2015-08-02 18:02:41 -0600

    removing minify from release.bat

--- bin/release.bat
+++ bin/release.bat
@@ -2,7 +2,6 @@
 :: Build the release files, commits any needed changes, and pushes the changes'
 :: to the remote repository
 call %~dp0make-release.bat
-call python %~dp0minify.py
 git add -A .
 git commit -m"releasing"
 git push
@@ -11,3 +10,4 @@ pushd %~dp0..\mtik00.github.io
 git add -A .
 git commit -m"releasing"
 git push
+popd
