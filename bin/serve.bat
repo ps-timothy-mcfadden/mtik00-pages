bfff8cc580f8caa90c6a2ef272c6d5a2b3667974
Timothy McFadden <timothy.mcfadden@avagotech.com>
2015-08-07 10:47:36 -0600

    adding Python server of static files

--- bin/serve.bat
+++ bin/serve.bat
@@ -1,9 +1,32 @@
 @echo off
 cd %~dp0..
 
+echo %1|findstr /xr "p" >nul && (
+  goto :python
+) || (
+  goto :hugo
+)
+
+:: =============================================================================
+:: Run python on the already-built static pages.  This is for checking the end
+:: result of the pipeline to ensure everything worked.
+:python
+pushd mtik00.github.io
+start python -m SimpleHTTPServer 8000
+popd
+goto :end
+:: =============================================================================
+
+:: =============================================================================
+:: Clean the Hugo build directory, then call Hugo to serve the content
+:hugo
 IF EXIST build (
     rmdir /q /s build
     sleep 1
 )
 
 start hugo server --watch --source="site" --bind="localhost"
+goto :end
+:: =============================================================================
+
+:end
